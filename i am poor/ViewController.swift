61ba7b8c9c62fd474c7a3c77b613bbd9b3f1d65f
Jared Cruz <jaredtronc@gmail.com>
2022-06-20 12:21:48 -0700

    Initial Commit

---
+++ i am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  i am poor
+//
+//  Created by Jared Cruz on 6/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
